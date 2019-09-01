f9e9bcf40f9b54988504fac1b3ea186a935d02fd
Damian Norman <damiannorman@gmail.com>
2019-09-01 14:26:47 -0400

    async actions

--- src/actions/appointmentAction.js
+++ src/actions/appointmentAction.js
@@ -1,10 +1,13 @@
 export const appointmentAction = (id) => {
 	return dispatch => {
-		dispatch({
-			type: 'GET_APPOINTMENT_ACTION',
-			payload: {
-				id
-			}
-		});
+		let t = setTimeout(e => {
+			clearTimeout(t);
+			dispatch({
+				type: 'GET_APPOINTMENT_ACTION',
+				payload: {
+					id
+				}
+			});
+		}, 1000);
 	};
 };
--- src/actions/appointmentsAction.js
+++ src/actions/appointmentsAction.js
@@ -1,33 +1,36 @@
 export const appointmentsAction = () => {
 	return dispatch => {
-		dispatch({
-			type: 'GET_APPOINTMENTS_ACTION',
-			payload: [
-				{
-					"created_at" : "2015-04-21T10:44:51-06:00",
-					"end" : "2015-04-24T07:30:00-06:00",
-					"id" : 695,
-					"patient_id" : 16,
-					"physician_id" : 14,
-					"start" : "2015-04-24T07:00:00-06:00",
-					"updated_at" : "2015-04-21T10:44:51-06:00",
-					"patient_name" :"Mike Ross",
-					"physician_name" : "Gregory House",
-					"notes": "patient is stressed, lacking confidence"
-				},
-				{
-					"created_at" : "2015-04-22T10:44:51-06:00",
-					"end" : "2015-04-25T07:30:00-06:00",
-					"id" : 696,
-					"patient_id" : 17,
-					"physician_id" : 14,
-					"start" : "2015-04-25T07:00:00-06:00",
-					"updated_at" : "2015-04-22T10:44:51-06:00",
-					"patient_name" :"Mike Ross",
-					"physician_name" : "Gregory House",
-					"notes": "patient is showing signs of depression"
-				}
-			]
-		})
+		let t = setTimeout(e => {
+			clearTimeout(t);
+			dispatch({
+				type: 'GET_APPOINTMENTS_ACTION',
+				payload: [
+					{
+						"created_at" : "2015-04-21T10:44:51-06:00",
+						"end" : "2015-04-24T07:30:00-06:00",
+						"id" : 695,
+						"patient_id" : 16,
+						"physician_id" : 14,
+						"start" : "2015-04-24T07:00:00-06:00",
+						"updated_at" : "2015-04-21T10:44:51-06:00",
+						"patient_name" :"Mike Ross",
+						"physician_name" : "Gregory House",
+						"notes": "patient is stressed, lacking confidence"
+					},
+					{
+						"created_at" : "2015-04-22T10:44:51-06:00",
+						"end" : "2015-04-25T07:30:00-06:00",
+						"id" : 696,
+						"patient_id" : 17,
+						"physician_id" : 14,
+						"start" : "2015-04-25T07:00:00-06:00",
+						"updated_at" : "2015-04-22T10:44:51-06:00",
+						"patient_name" :"Mike Ross",
+						"physician_name" : "Gregory House",
+						"notes": "patient is showing signs of depression"
+					}
+				]
+			})
+		}, 1000);
 	};
 };
