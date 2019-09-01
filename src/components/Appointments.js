896d9c74df2713e7d3e44ea965884f3507945933
Damian Norman <damiannorman@gmail.com>
2019-09-01 11:55:43 -0400

    physician tooltip

--- src/components/Appointments.js
+++ src/components/Appointments.js
@@ -1,65 +1,86 @@
-import React from "react";
+import React, {Component} from 'react';
 import ReactTable from "react-table";
 import "react-table/react-table.css";
 import PropTypes from 'prop-types';
 import {getDate, getTime} from "../Utils";
 
-const Appointments = (props) => {
-	let result;
-	if (props.appointments.length) {
-		result = (<div>Appointments ok</div>);
-	} else {
-		result = (<div>Appointments no data</div>);
+import ReactTooltip from "react-tooltip";
+
+class Appointments extends Component {
+	constructor(props) {
+		super(props);
+		let result;
+		if (props.appointments.length) {
+			result = (<div>Appointments ok</div>);
+		} else {
+			result = (<div>Appointments no data</div>);
+		}
 	}
 
-	const { appointments = [], dateFormat, timeFormat, navigate } = props;
+	render() {
+		const { appointments = [], dateFormat, timeFormat, navigate } = this.props;
 
-	return (
-		<div className="Appointments">
-			<ReactTable
-				data={appointments}
-				defaultPageSize={10}
-				className="-striped -highlight"
-				getTrProps={(state, rowInfo) => {
-					if (rowInfo && rowInfo.row) {
-						return {
-							onClick: (e) => {
-								navigate(`/appointment/${rowInfo.row._original.id}`);
+		return (
+			<div className="Appointments">
+				<ReactTooltip />
+				<ReactTable
+					data={appointments}
+					defaultPageSize={10}
+					className="-striped -highlight"
+					getTrProps={(state, rowInfo) => {
+						if (rowInfo && rowInfo.row) {
+							return {
+								onClick: (e) => {
+									navigate(`/appointment/${rowInfo.row._original.id}`);
+								}
+							}
+						}else{
+							return {}
+						}
+					}}
+
+					columns={[
+						{
+							Header: "Name",
+							id: "name",
+							accessor: d => d.patient_name
+						},
+						{
+							Header: "Physician",
+							id: "physician",
+							accessor: d => d.physician_name,
+							Cell: row => {
+								let fooRef;
+								return (
+									<div onMouseEnter={e => {
+										ReactTooltip.show(fooRef);
+									}} onMouseLeave={e => {
+										ReactTooltip.hide(fooRef);
+									}}>
+										<div style={{ "position": "absolute" }} ref={ref => fooRef = ref} data-tip={row.value}></div>
+										{row.value}
+									</div>
+								);
+							}
+						},
+						{
+							Header: "Created",
+							id: "created",
+							accessor: d => {
+								return (`${getDate(d.created_at)}, ${getTime(d.created_at)}`)
+							}
+						},
+						{
+							Header: "Updated",
+							id: "updated",
+							accessor: d => {
+								return (`${getDate(d.updated_at)}, ${getTime(d.updated_at)}`)
 							}
 						}
-					}else{
-						return {}
-					}
-				}}
-
-				columns={[
-					{
-						Header: "Name",
-						id: "name",
-						accessor: d => d.patient_name
-					},
-					{
-						Header: "Physician",
-						id: "physician",
-						accessor: d => d.physician_name
-					},
-					{
-						Header: "Created",
-						id: "created",
-						accessor: d => {
-							return (`${getDate(d.created_at)}, ${getTime(d.created_at)}`)
-						}
-					},
-					{
-						Header: "Updated",
-						id: "updated",
-						accessor: d => {
-							return (`${getDate(d.updated_at)}, ${getTime(d.updated_at)}`)
-						}
-					}
-				]}
-			/>
-		</div>);
+					]}
+				/>
+			</div>);
+	}
 };
 
 Appointments.propTypes = {
