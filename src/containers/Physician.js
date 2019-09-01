103c213b42fdb130a67fbeef933af137d8c3c94b
Damian Norman <damiannorman@gmail.com>
2019-09-01 02:19:33 -0400

    profile page

--- src/containers/Physician.js
+++ src/containers/Physician.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
+import AppHeader from "../components/AppHeader";
+import AppFooter from "../components/AppFooter";
 
 class Physician extends Component {
 	render() {
 		return (
-			<div className="container">
-				Physician
+			<div>
+				<AppHeader title={"Physician."}
+						   selected={'profile'} />
+				<AppFooter />
 			</div>
 		);
 	}
